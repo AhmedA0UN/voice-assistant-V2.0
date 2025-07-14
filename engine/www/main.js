a89769645fc2257fe346dd037c0ac391aa611a96
Ahmed <ahmed.aoun@isimg.tn>
2025-07-14 16:58:51 +0100

    ajouter des lignes de code

---
+++ engine/www/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.text').textllate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: 'bounceIn',
+        },
+        out: {
+            effect: 'bounceOut',
+        },
+    });
+});
